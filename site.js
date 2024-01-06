08b673154eadbd9fbea8e16b038adfc1ce2f0f82
Imran1128 <138041710+Imran1128@users.noreply.github.com>
2024-01-07 00:27:08 +0600

    Update site.js

--- site.js
+++ site.js
@@ -6,7 +6,6 @@
         var age = document.getElementById("age").value;
 
         if (age === "") {
-            // Show both divs when the input field is empty
             adultCard.style.display = "block";
             teenCard.style.display = "block";
         } else if (age < 18) {
